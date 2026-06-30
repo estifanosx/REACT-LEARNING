8b330a81b26714e49af7778f31051074267a4239
estifanosX <natim6316@gmail.com>
2026-06-30 09:43:38 -0700

    feat: add state management for tasks and input in Task component

--- src/Components/Components/excersise/Task.jsx
+++ src/Components/Components/excersise/Task.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 export default function Task() {
+ const [task ,useTask] =useState([])
+const [input ,useInput] =useState([])
   return (
     <div className="min-h-screen bg-neutral-700 text-white ">
       <header className="mb-20">
@@ -9,7 +11,7 @@ export default function Task() {
       <form className="flex items-center justify-between gap-6">
         <input className=" flex-1 type-text border border-slate-400 p-4 " placeholder="Write your task here ........" >
         </input>
-        <button className="bg-purple-700 p-3 rounded-2xl hover:cursor-pointer">
+        <button  className="bg-purple-700 p-3 rounded-2xl hover:cursor-pointer">
          Add task
         </button>
       </form>
