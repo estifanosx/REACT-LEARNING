507ecb96a92345b8c5332a10f6558e9d12db45b6
estifanosX <natim6316@gmail.com>
2026-06-30 17:32:14 -0700

    fix: correct input handling and structure in Form component

--- src/Components/Components/excersise/Form.jsx
+++ src/Components/Components/excersise/Form.jsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 export default function Form() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  function submithandle(e) {
+    e.preventDefault();
+    console.log(email)
+    console.log(password);
+  }
   return (
-    <form className="min-h-screen bg-neutral-900 flex items-center justify-center">
+    <form
+      className="min-h-screen bg-neutral-900 flex items-center justify-center"
+      onSubmit={submithandle}
+    >
       <div className="bg-neutral-800 p-8 rounded-xl w-96 space-y-5 ">
         <header className="text-white text-5xl">
           <h2>Sign Up</h2>
@@ -16,22 +25,26 @@ export default function Form() {
               placeholder="write your email"
               id="email"
               className="w-full p-3 rounded border border-neutral-600 bg-neutral-700 text-white"
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <label for="email">Email</label>
+            <label htmlFor="email">Email</label>
           </div>
 
           <div lassName="gap-4">
-            {" "}
             <input
               type="password"
               placeholder="........"
               id="password"
               className="w-full p-3 rounded border border-neutral-600 bg-neutral-700 text-white"
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <label for="password">password</label>
+            <label htmlFor="password">password</label>
           </div>
         </div>
-        <button className="text-2xl text-white bg-emerald-500 p-4 rounded-xl ">
+        <button
+          className="text-2xl text-white bg-emerald-500 p-4 rounded-xl  "
+          type="submit"
+        >
           Submit
         </button>
       </div>
