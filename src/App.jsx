3ba7b6e73d0a015fee593b704ace7f3c63ae1b88
estifanosX <natim6316@gmail.com>
2026-06-30 17:05:29 -0700

    feat: add Form component for user sign-up functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Counter from "./Components/Components/excersise/Counter";
 import Task from "./Components/Components/excersise/Task";
+import Form from "./Components/Components/excersise/Form";
 export default function App() {
   return (
     <div>
       {/* <Counter /> */}
-      <Task />
+      {/* <Task /> */}
+      <Form />
     </div>
   );
 }
---
+++ src/Components/Components/excersise/Form.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Form() {
+  return (
+    <form className="min-h-screen bg-neutral-900 flex items-center justify-center">
+      <div className="bg-neutral-800 p-8 rounded-xl w-96 space-y-5 ">
+        <header className="text-white text-5xl">
+          <h2>Sign Up</h2>
+        </header>
+        <div className="y-5 gap-4">
+          <div className="gap-4">
+            <input
+              type="email"
+              placeholder="write your email"
+              id="email"
+              className="w-full p-3 rounded border border-neutral-600 bg-neutral-700 text-white"
+            />
+            <label for="email">Email</label>
+          </div>
+
+          <div lassName="gap-4">
+            {" "}
+            <input
+              type="password"
+              placeholder="........"
+              id="password"
+              className="w-full p-3 rounded border border-neutral-600 bg-neutral-700 text-white"
+            />
+            <label for="password">password</label>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
